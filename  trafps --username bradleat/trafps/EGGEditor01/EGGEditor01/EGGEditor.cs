0e27b1680d934eb1c4cf83f8cbc6191d088ca1fc
evbevmagevbof <evbevmagevbof@2ace7df3-e853-0410-ba91-43af2b924171>
2008-11-23 21:33:09 +0000

    Added EGGEditor to the svn. (Properly)

---
+++  trafps --username bradleat/trafps/EGGEditor01/EGGEditor01/EGGEditor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EGGEditor01
+{
+
+    public partial class EGGEditor : Form
+    {
+        public EGGEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void EGGEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IntPtr getDrawSurface()  
+        {  
+            return pctSurface.Handle;  
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void propertyGrid1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctSurface_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void aDDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }  
+        
+        
+    }
+}
---
+++  trafps --username bradleat/trafps/EGGEditor01/EGGEditor01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TRA_Game;
+
+namespace EGGEditor01
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+                  EGGEditor form = new EGGEditor();  
+                  form.Show();  
+                  Game1 game = new Game1(form.getDrawSurface());  
+                  game.Run();              
+       }  
+    }
+}
+
